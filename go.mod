ae268a3194de5945e9ac8f407bf654b4aaa442da
Josh Dolitsky <josh@dolit.ski>
2023-11-14 15:30:59 -0600

    initial version
    Signed-off-by: Josh Dolitsky <josh@dolit.ski>

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/oci-playground/specs-latest
+
+go 1.21.4
+
+require (
+	github.com/xeipuuv/gojsonschema v1.2.0
+	gopkg.in/yaml.v2 v2.4.0
+)
+
+require (
+	github.com/opencontainers/distribution-spec v1.0.1 // indirect
+	github.com/opencontainers/image-spec v1.0.2 // indirect
+	github.com/opencontainers/runtime-spec v1.0.1 // indirect
+	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
+	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
+)
---
+++ main.go
@@ -0,0 +1,155 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	"gopkg.in/yaml.v2"
+)
+
+const (
+	specsConfigFilename = "specs.yaml"
+)
+
+type (
+	specsConfig struct {
+		Specs []specsConfigSpec `yaml:"specs"`
+	}
+
+	specsConfigSpec struct {
+		Name     string               `yaml:"name"`
+		Remote   string               `yaml:"remote"`
+		Releases []specsConfigRelease `yaml:"releases"`
+	}
+
+	specsConfigRelease struct {
+		Commit string `yaml:"commit"`
+		Tag    string `yaml:"tag"`
+		Branch string `yaml:"branch"`
+	}
+)
+
+func main() {
+	b, err := os.ReadFile(specsConfigFilename)
+	if err != nil {
+		log.Fatalf("ReadFile: %v", err)
+	}
+	config := specsConfig{}
+	if err := yaml.Unmarshal(b, &config); err != nil {
+		log.Fatalf("Unmarshal: %v", err)
+	}
+	processConfig(&config)
+}
+
+func absPath() string {
+	currentDir, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("os.Getwd: %v", err)
+	}
+	currentAbsDir, err := filepath.Abs(currentDir)
+	if err != nil {
+		log.Fatalf("filepath.Abs: %v", err)
+	}
+	log.Printf("current directory: %s", currentAbsDir)
+	return currentAbsDir
+}
+
+func processConfig(config *specsConfig) {
+	currentDir := absPath()
+	gitWorkspace := filepath.Join(currentDir, "docs", "git-workspace")
+	log.Printf("ensuring git workspace directory %s", gitWorkspace)
+	os.MkdirAll(gitWorkspace, 0755)
+	for _, spec := range config.Specs {
+		os.Chdir(gitWorkspace)
+		processSpec(&spec)
+
+	}
+	os.Chdir(currentDir)
+}
+
+func processSpec(spec *specsConfigSpec) {
+	currentDir := absPath()
+	log.Printf("[%s] begin processing, https git remote: %s", spec.Name, spec.Remote)
+	base := strings.TrimSuffix(filepath.Base(spec.Remote), ".git")
+	specWorkspace := filepath.Join(currentDir, base)
+	if _, err := os.Stat(specWorkspace); os.IsNotExist(err) {
+		cmd := exec.Command("git", "clone", "--depth=1000000", spec.Remote, specWorkspace)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err := cmd.Run()
+		if err != nil {
+			log.Fatalf("cmd.Run: %v", err)
+		}
+	}
+	specOutputParentDir := filepath.Join(currentDir, "..", "specs", spec.Name)
+	os.MkdirAll(specOutputParentDir, 0755)
+	for i, release := range spec.Releases {
+		log.Printf("[%s] count:%d tag:%s branch:%s commit:%s", spec.Name, i+1, release.Commit, release.Tag, release.Branch)
+		checkoutTarget := release.Tag
+		if checkoutTarget == "" {
+			checkoutTarget = release.Commit
+			if checkoutTarget == "" {
+				log.Fatalf("[%s] invalid config for release at index %d", spec.Name, i)
+			}
+		}
+
+		specOutputDir := filepath.Join(specOutputParentDir, checkoutTarget)
+		if _, err := os.Stat(specOutputDir); !os.IsNotExist(err) {
+			log.Printf("[%s] output folder %s exists, skipping", spec.Name, specOutputDir)
+			continue
+		}
+
+		log.Printf("[%s] changing dir to %s", spec.Name, currentDir)
+		os.Chdir(specWorkspace)
+
+		log.Printf("[%s] running \"git checkout -f %s\"", spec.Name, checkoutTarget)
+		cmd := exec.Command("git", "checkout", "-f", checkoutTarget)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			log.Fatalf("cmd.Run: %v", err)
+		}
+		log.Printf("[%s] running \"git clean -f %s\"", spec.Name, checkoutTarget)
+		cmd = exec.Command("git", "clean", "-f", checkoutTarget)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			log.Fatalf("cmd.Run: %v", err)
+		}
+		log.Printf("[%s] running \"make docs\"", spec.Name)
+
+		// Ideally "make docs" should be all we need...
+		cmd = exec.Command("make", "docs")
+
+		// Various hacks for specific specs... DONT JUDGE ME
+		switch spec.Name {
+		case "image":
+			cmd = exec.Command("sh", "-c", fmt.Sprintf("cat Makefile | sed 's/-it/-i/g' | sed 's|-f gfm|-f gfm --metadata title=\"Open Container Initiative Image Format Specification\"|g' > Makefile.new && (make -f Makefile.new docs || (cd .tool/ && go get github.com/opencontainers/image-spec/specs-go@%s && cd ../ && make -f Makefile.new docs))", checkoutTarget))
+		case "distribution":
+			cmd = exec.Command("sh", "-c", fmt.Sprintf("cat Makefile | sed 's/-it/-i/g' | sed 's/go mod init \\&\\& \\\\/\\(rm -f go.* \\&\\& go mod init main \\)\\&\\& \\\\/g' | sed 's|-f gfm|-f gfm --metadata title=\"Open Container Initiative Distribution Specification\"|g'> Makefile.new && (make -f Makefile.new docs || (cd .tool/ && go get github.com/opencontainers/distribution-spec/specs-go@%s && cd ../ && make -f Makefile.new docs)) && rm -f output/.gitkeep", checkoutTarget))
+		case "runtime":
+			cmd = exec.Command("sh", "-c", fmt.Sprintf("cat Makefile | sed 's/-it/-i/g' | sed 's/go mod init \\\\/go mod init main \\\\/g' > Makefile.new && (make -f Makefile.new docs || (cd .tool/ && go get github.com/opencontainers/runtime-spec/specs-go@%s && cd ../ && make -f Makefile.new docs))", checkoutTarget))
+		}
+
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err := cmd.Run()
+		os.Remove("Makefile.new")
+		if err != nil {
+			log.Fatalf("cmd.Run: %v", err)
+		}
+
+		// move the output contents
+		log.Printf("[%s] moving output/ to %s", spec.Name, specOutputDir)
+		if err := os.Rename("output/", specOutputDir); err != nil {
+			log.Fatalf("os.Rename: %v", err)
+		}
+	}
+	log.Printf("[%s] changing dir to %s", spec.Name, currentDir)
+	os.Chdir(currentDir)
+	log.Printf("[%s] end processing", spec.Name)
+}
